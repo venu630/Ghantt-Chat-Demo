46431400a2bfc62b04a3ec35b06fadb74cd3da36
Venu <sirisanagandlavenu@gmail.com>
2023-07-20 00:29:23 +0530

    pushing all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import * as React from "react";
+import {
+  GanttComponent,
+  Inject,
+  Selection,
+  ColumnsDirective,
+  ColumnDirective,
+  Toolbar, ToolbarItem, Edit, EditSettingsModel
+} from "@syncfusion/ej2-react-gantt";
+import { projectNewData } from "./data";
+function Default() {
+  const taskFields = {
+    id: "TaskID",
+    name: "TaskName",
+    startDate: "StartDate",
+    endDate: "EndDate",
+    duration: "Duration",
+    progress: "Progress",
+    dependency: "Predecessor",
+    child: "subtasks",
+  };
+  const editOptions = {
+    allowAdding: true,
+    allowEditing: true,
+    allowDeleting: true
+};
+const toolbarOptions = ['Add','Edit','Delete','Cancel','Update','PrevTimeSpan','NextTimeSpan','ExpandAll','CollapseAll','Search','Indent','Outdent'];
+  // const projectStartDate = new Date("03/24/2019");
+  // const projectEndDate = new Date("07/06/2019");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="control-pane">
+      <div className="control-section">
+        <GanttComponent
+          id="Default"
+          dataSource={projectNewData}
+          height="100vh"
+          style={{height: "100vh"}}
+          taskFields={taskFields}
+          // enableImmutableMode={true}
+          // treeColumnIndex={1}
+          // labelSettings={labelSettings}
+          // projectStartDate={projectStartDate}
+          // projectEndDate={projectEndDate}
+          editSettings={editOptions} toolbar={toolbarOptions}
         >
-          Learn React
-        </a>
-      </header>
+          <ColumnsDirective>
+            <ColumnDirective field="TaskID" width="80"></ColumnDirective>
+            <ColumnDirective
+              field="TaskName"
+              headerText="Job Name"
+              width="300"
+              clipMode="EllipsisWithTooltip"
+            ></ColumnDirective>
+            <ColumnDirective field="StartDate" format="dd-MMM-yyyy"></ColumnDirective>
+            <ColumnDirective field="Duration"></ColumnDirective>
+            <ColumnDirective field="Progress"></ColumnDirective>
+          </ColumnsDirective>
+          <Inject services={[Selection, Toolbar, Edit]} />
+        </GanttComponent>
+      </div>
     </div>
   );
 }
-
-export default App;
+export default Default;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { registerLicense } from '@syncfusion/ej2-base';
 
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NGaF5cXmdCeUx0Q3xbf1xzZFxMZF1bRnVPMyBoS35RdUVlW3leeXBURGRYV0Z0');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
